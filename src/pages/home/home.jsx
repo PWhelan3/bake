328185e28a7dfe264701558015807cceef2fabbf
Peter <pwhelanstudio@gmail.com>
2024-05-26 18:42:09 +0100

    Add: Home page components

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const home = () => {
   return (
-    <div>home</div>
+    <div>
+      <navbar />
+      <hero />
+      <featured />
+      <items />
+      <special />
+      <footer />
+    </div>
   )
 }
 
