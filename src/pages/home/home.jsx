6d49efa450a8dd0d4e068c88e425f1c25edbe81d
Peter <pwhelanstudio@gmail.com>
2024-05-27 23:12:04 +0100

    Add: Components

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { Navbar } from '.../components/navbar/Navbar';
 
 const home = () => {
   return (
     <div>
-      <navbar />
+      <Navbar />
       <hero />
       <featured />
       <items />
