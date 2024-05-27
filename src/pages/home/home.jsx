71029434ed66c872fa0eee4f72cfd519b033206e
Peter <pwhelanstudio@gmail.com>
2024-05-27 23:17:13 +0100

    Add: Component imports

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Navbar } from '.../components/navbar/Navbar';
+import { Cart } from '.../components/cart/Cart';
+import { Featured } from '.../components/featured/Featured';
+import { Footer } from '.../components/footer/Footer';
+import { Hero } from '.../components/hero/Hero';
+import { Items } from '.../components/items/Items';
+import { Special } from '.../components/special/Special';
 
 const home = () => {
   return (
     <div>
       <Navbar />
-      <hero />
-      <featured />
-      <items />
-      <special />
-      <footer />
+      <Hero />
+      <Featured />
+      <Items />
+      <Special />
+      <Footer />
     </div>
   )
 }
