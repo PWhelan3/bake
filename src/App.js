3d27833590db4b9bfe9d003110763dc6bae9d8c5
Peter <pwhelanstudio@gmail.com>
2024-05-30 22:52:25 +0100

    Add: Routing to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import styles from './app.module.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Layout } from "./pages/layout/Layout";
+import { Home } from "./pages/home/Home";
+import { Shop } from "./pages/shop/Shop";
+import { About } from "./pages/about/About";
+import { Checkout } from "./pages/checkout/Checkout";
+import { NoPage } from "./pages/nopage/NoPage";
 
 function App() {
   return (
     <div className={styles.app}>
       <p>Hello!</p>
-      <Home />
-      <Shop />
-      <About />
-      <Checkout />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="shop" element={<Shop />} />
+            <Route path="about" element={<About />} />
+            <Route path="checkout" element={<Checkout />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
