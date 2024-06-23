1dbc975d3f5abcdc47b6e830a7ac567c880c2281
Peter <pwhelanstudio@gmail.com>
2024-06-23 22:51:31 +0100

    More edits

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -22,8 +22,7 @@ function ShoppingCart({
 				<div className={styles.cart-products}>
 					{products.length === 0 && (
 						<span className={styles.empty-text}>
-							Your basket is
-							currently empty
+							Your basket is currently empty
 						</span>
 					)}
 					{products.map((product) => (
@@ -34,66 +33,29 @@ function ShoppingCart({
 									{product.name}
 								</h3>
 								<span className={styles.product-price}>
-									{product.price *
-										product.count}
-									$
+									{product.price * product.count} $
 								</span>
 							</div>
-							<select
-								className={styles.count}
-								value={
-									product.count
-								}
-								onChange={(
-									event
-								) => {
-									onQuantityChange(
-										product.id,
-										event
-											.target
-											.value
-									);
-								}}>
-								{[
-									...Array(
-										10
-									).keys(),
-								].map(
-									(number) => {
-										const num =
-											number +
-											1;
+							<select className={styles.count} value={product.count} onChange={(event) => {onQuantityChange(product.id, event.target.value); }}>
+								{[...Array(10).keys(),].map((number) => {
+										const num = number + 1;
 										return (
-											<option
-												value={
-													num
-												}
-												key={
-													num
-												}>
-												{
-													num
-												}
+							        <option value={num} key={num}>
+												{num}
 											</option>
 										);
 									}
 								)}
 							</select>
 							<button
-								className="btn remove-btn"
-								onClick={() =>
-									onProductRemove(
-										product
-									)
-								}>
-								<RiDeleteBin6Line
-									size={20}
-								/>
+								className={`${styles.btn} ${styles.remove-btn}`}
+								onClick={() => onProductRemove(product)}>
+								<RiDeleteBin6Line size={20}/>
 							</button>
 						</div>
 					))}
 					{products.length > 0 && (
-						<button className="btn checkout-btn">
+						<button className={`${styles.btn} ${styles.checkout-btn}`}>
 							Proceed to checkout
 						</button>
 					)}
