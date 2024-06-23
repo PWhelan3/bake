0cadf0f6ae36065c5fa473366f2fb9f9482f1a95
Peter <pwhelanstudio@gmail.com>
2024-06-23 22:39:16 +0100

    Rewriting classNames

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,63 +1,123 @@
-import React from 'react';
-import { AiFillCloseCircle, AiOutlineShopping } from "react-icons/ai";
-import {styles} from "./Cart.module.css";
+import React from "react";
+import styles from './Cart.module.css';
+import { AiFillCloseCircle } from "react-icons/ai";
 import { RiDeleteBin6Line } from "react-icons/ri";
 
-
-
-const Cart = ({
-  visibility,
-  products,
-  onProductRemove,
-  onClose,
-  onQuantityChange,
-}) => {
-  return (
-    <div className={styles.modal}>
-      <div className={styles.header}>
-        <h2>Shopping Cart</h2>
-        <button className={`${styles.btn} ${styles.close-btn}`} onClick={onClose}>
-          <AiFillCloseCircle size={30} />
-        </button>
-      </div>
-      <div className={styles.cart-products}>
-          {products.length === 0 && (<span className={styles.empty-text}>
-            Your cart is currently empty
-            </span>)}
-            {products.map(product => (
-              <div className={styles.cart-product} key={product.id}>
-                <img
-                src={product.inage}
-                alt={product.name}
-                />
-                <div className={styles.product-info}>
-                  <h3>
-                    {product.name}
-                  </h3>
-                  <span className={styles.product-price}>
-                    {product.price * product.count}$
-                  </span>
-                </div>
-                <select className={styles.count} value={product.count} onChange={(event) => {
-                  onQuantityChange(product.id, event.target.value);
-                }}>
-                  {
-                    [...Array(10).keys()].map(number => {
-                      const num = number + 1;
-                      return <option value={num} key={num}>{num}</option>
-                    })
-                  }
-                </select>
-                <button className={`${styles.btn} ${styles.remove-btn}`} onClick={() => onProductRemove(product)}>
-                  <RiDeleteBin6Line size={20}/>
-                </button>
-              </div>
-            ))}
-            {products.length > 0 && <button className={`${styles.btn} ${styles.checkout-btn}`}>Proceed to checkout</button>}
-        </div>
-    </div>
-
-  )
+function ShoppingCart({
+	visibilty,
+	products,
+	onProductRemove,
+	onClose,
+	onQuantityChange,
+}) {
+	return (
+		<div
+			className={styles.modal}
+			style={{
+				display: visibilty
+					? "block"
+					: "none",
+			}}>
+			<div className={styles.shoppingCart}>
+				<div className={styles.header}>
+					<h2>Shopping cart</h2>
+					<button
+						className={`${styles.btn} ${styles.close-btn}`}
+						onClick={onClose}>
+						<AiFillCloseCircle
+							size={30}
+						/>
+					</button>
+				</div>
+				<div className="cart-products">
+					{products.length === 0 && (
+						<span className="empty-text">
+							Your basket is
+							currently empty
+						</span>
+					)}
+					{products.map((product) => (
+						<div
+							className="cart-product"
+							key={product.id}>
+							<img
+								src={
+									product.image
+								}
+								alt={product.name}
+							/>
+							<div className="product-info">
+								<h3>
+									{product.name}
+								</h3>
+								<span className="product-price">
+									{product.price *
+										product.count}
+									$
+								</span>
+							</div>
+							<select
+								className="count"
+								value={
+									product.count
+								}
+								onChange={(
+									event
+								) => {
+									onQuantityChange(
+										product.id,
+										event
+											.target
+											.value
+									);
+								}}>
+								{[
+									...Array(
+										10
+									).keys(),
+								].map(
+									(number) => {
+										const num =
+											number +
+											1;
+										return (
+											<option
+												value={
+													num
+												}
+												key={
+													num
+												}>
+												{
+													num
+												}
+											</option>
+										);
+									}
+								)}
+							</select>
+							<button
+								className="btn remove-btn"
+								onClick={() =>
+									onProductRemove(
+										product
+									)
+								}>
+								<RiDeleteBin6Line
+									size={20}
+								/>
+							</button>
+						</div>
+					))}
+					{products.length > 0 && (
+						<button className="btn checkout-btn">
+							Proceed to checkout
+						</button>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Cart
+export default ShoppingCart;
