84fab2c31975443cb6c3b0dba48c5c518728e325
Peter <pwhelanstudio@gmail.com>
2024-06-28 13:14:55 +0100

    Edit: App.js layout

--- src/App.js
+++ src/App.js
@@ -111,7 +111,59 @@ function App() {
           <GiShoppingBag size={24}/>
         </button>
       </div>
-      <main>
+	  <main>
+				<h2 className="title">
+					Products
+				</h2>
+				<div className="products">
+					{products.map((product) => (
+						<div
+							className="product"
+							key={product.id}>
+							<img
+								className="product-image"
+								src={
+									product.image
+								}
+								alt={
+									product.image
+								}
+							/>
+							<h4 className="product-name">
+								{product.name}
+							</h4>
+							<RatingStars
+								rating={
+									product.rating
+								}
+							/>
+							<p>
+								{
+									product.description
+								}
+							</p>
+							<span className="product-price">
+								{product.price}$
+							</span>
+							<div className="buttons">
+								<button className="btn">
+									Detail
+								</button>
+								<button
+									className="btn"
+									onClick={() =>
+										addProductToCart(
+											product
+										)
+									}>
+									Add to cart
+								</button>
+							</div>
+						</div>
+					))}
+				</div>
+			</main>
+      <div>
       <p>Hello!</p>
       <BrowserRouter>
         <Routes>
@@ -124,7 +176,7 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
-      </main>
+      </div>
     </div>
   );
 }
