cd5667b7e5db2d51eee974aca23560cf8ead7623
Peter <pwhelanstudio@gmail.com>
2024-07-01 16:09:38 +0100

    Create: Product component

--- src/App.js
+++ src/App.js
@@ -10,63 +10,8 @@ import { Shop } from "./pages/shop/Shop";
 import { About } from "./pages/about/About";
 import { Checkout } from "./pages/checkout/Checkout";
 import { NoPage } from "./pages/nopage/NoPage";
+import { products } from './data/Products';
 
-const products = [
-	{
-		id: 1,
-		name: "Vivamus vitae",
-		rating: 4.3,
-		description:
-			"Vivamus vitae neque accumsan, ultrices nisl et, viverra magna. Fusce nec maximus sem.",
-		price: 199,
-		image: require("./assets/images/product-1.png"),
-	},
-	{
-		id: 2,
-		name: "Fusce sit amet ipsum",
-		rating: 4.2,
-		description:
-			"Maecenas fermentum urna egestas urna ullamcorper sodales. Sed a enim imperdiet, tempus massa a, iaculis tellus.",
-		price: 229,
-		image: require("./assets/images/product-2.png"),
-	},
-	{
-		id: 3,
-		name: "Etiam volutpat aliquam",
-		rating: 3.2,
-		description:
-			"Praesent et orci vel nunc interdum aliquet et non dolor. Etiam eget finibus justo",
-		price: 99,
-		image: require("./assets/images/product-3.png"),
-	},
-	{
-		id: 4,
-		name: "Lorem ipsum dolor",
-		rating: 4.8,
-		description:
-			"Duis nibh sapien, placerat non nulla ac, suscipit laoreet tortor.",
-		price: 119,
-		image: require("./assets/images/product-4.png"),
-	},
-	{
-		id: 5,
-		name: "Ultrices nisl",
-		rating: 4.5,
-		description:
-			"Phasellus condimentum, ante et dictum placerat, nulla ipsum commodo lorem, ut mollis nibh turpis a metus.",
-		price: 85,
-		image: require("./assets/images/product-5.jpg"),
-	},
-	{
-		id: 6,
-		name: "Curabitur in elementum tortor",
-		rating: 3.8,
-		description:
-			" Mauris convallis diam nibh, non malesuada enim facilisis non. Etiam sapien augue, molestie a porta sed",
-		price: 149,
-		image: require("./assets/images/product-6.png"),
-	},
-];
 
 function App() {
   const [cartsVisibility, setCartVisible] = useState(false);
