da28fa49e1a7f561a2da65865863ce4a3d757558
Peter <pwhelanstudio@gmail.com>
2024-07-16 21:44:45 +0100

    Edit: Testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <div>
+      <div>
+        <div>
+          <h3></h3>
+          <img></img>
+          <p></p>
+        </div>
+        <div>
+          <h3></h3>
+          <img></img>
+          <p></p>
+        </div>
+        <div>
+          <h3></h3>
+          <img></img>
+          <p></p>
+        </div>
+      </div>
+    </div>
   )
 }
 
