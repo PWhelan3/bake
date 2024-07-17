f3ec5f5d769964f4fa72d0a97cd2cd9e0c71d004
Peter <pwhelanstudio@gmail.com>
2024-07-17 13:22:22 +0100

    Edit: Dropdown Menu

---
+++ src/components/dropdown/Dropdown.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from './Dropdown.module.css';
+
+const Dropdown = () => {
+  return (
+    <div className={styles.container}>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Dropdown
